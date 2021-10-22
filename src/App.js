6c07040012e8f471a067403a873a51394ba4462f
manelnaghmouch <manel.naghmouchi@sesame.com.tn>
2021-10-22 10:39:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import MainRoute from "./Routes/MainRoute";
 import "./assets/Style.scss"
 import "./assets/css/style.css";
+import ReactGA from 'react-ga';
+import RouteChangeTracker from './RouteChangeTracker';
 
-
-
+const TRACKING_ID = "G-LLCB7Y2YTH"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
     <div>
- 
+      <RouteChangeTracker/>
       <MainRoute />
     </div>
   );
